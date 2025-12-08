1c78ea461a2bd155f2f91ed2418cda1c07efd168
krishna jeena02 <krishjeena8700745775@gmail.com>
2025-12-08 14:11:38 +0530

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ mongoose.connect(process.env.MONGODB_URL, {
     .catch((err) => console.error("MongoDB connection error:", err));
 
 // Mount router
-app.use("/api", quoteRouter); // http://localhost:3000/quote
+app.use("/", quoteRouter); // http://localhost:3000/quote
 
 // Error handling middleware
 app.use((err, req, res, next) => {
