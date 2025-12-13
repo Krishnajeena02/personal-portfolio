573e64459ae6a1c8ba6b62cf61fe7d18606ad641
krishna jeena02 <krishjeena8700745775@gmail.com>
2025-12-13 19:41:09 +0530

    updated backend

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173", // Allow Vite frontend
+    origin: "*", // Allow Vite frontend
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
